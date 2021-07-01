3d185de962fbc47f876d522443fbed60e452fef8
Enrico Graziani <enrico.graziani@cazoo.co.uk>
2021-07-01 11:29:51 +0100

    mob next [ci-skip]

--- src/main/example.ts
+++ src/main/example.ts
@@ -4,21 +4,24 @@ interface AccountService {
     printStatement(): void
 }
 
-export type Now = () => Date
-export type Display = (text: string) => void
+export interface Now {}
+
+export interface Display {
+    show(text: string): void;
+}
 
 export class ErikoAccountService implements AccountService {
-    constructor(private readonly now: Now, private readonly display: Display) {
+    constructor(private readonly now: Now, private readonly display: Display) { }
 
-    }
     deposit(amount: number): void {
-        throw new Error("Method not implemented.");
     }
     withdraw(amount: number): void {
-        throw new Error("Method not implemented.");
     }
     printStatement(): void {
-        throw new Error("Method not implemented.");
+        this.display.show(`Date       || Amount || Balance
+14/01/2012 || -500   || 2500
+13/01/2012 || 2000   || 3000
+10/01/2012 || 1000   || 1000`)
     }
 
 }
--- step-definitions/bank-account.step.ts
+++ step-definitions/bank-account.step.ts
@@ -1,22 +1,42 @@
 import { binding, given, then, when} from 'cucumber-tsflow';
-import { assert } from 'chai';
-import { Now } from '../src/main/example';
+import { assert, expect } from 'chai';
+import {Display, ErikoAccountService, Now} from '../src/main/example';
 
-const mockNow = (): Now => () => new Date()
+
+class NowMock implements Now{
+    set(date: string) {
+
+    }
+}
+
+class DisplayMock implements Display {
+    private text: string | undefined;
+
+    show(text: string) {
+        this.text = text;
+    }
+
+    shouldHaveBeenCalledWith(expectedText: string) {
+        expect(this.text).to.equal(expectedText);
+    }
+}
+
+const nowMock = new NowMock();
+const displayMock = new DisplayMock();
 
 @binding()
 export class BankAccountSteps {
-    private readonly accountService = new ErikoAccountService();
+    private readonly accountService = new ErikoAccountService(nowMock, displayMock);
 
     @given(/(a client makes )?a deposit of (\d+) on ([\d-]+)/)
     public givenDeposit(_: string, amount: string, date: string) {
-        // TODO mock date?
+        nowMock.set(date)
         this.accountService.deposit(Number(amount))
     }
 
     @given(/(a client makes )?a withdrawal of (\d*) on ([\d-]+)/)
     public givenWithdrawal(_:string, amount: string, date: string) {
-        // TODO mock date?
+        nowMock.set(date)
         this.accountService.withdraw(Number(amount))
     }
 
@@ -27,6 +47,6 @@ export class BankAccountSteps {
 
     @then(/they would see:/)
     public seeAccount(table: string) {
-        // mock console.log?
+        displayMock.shouldHaveBeenCalledWith(table);
     }
 }
